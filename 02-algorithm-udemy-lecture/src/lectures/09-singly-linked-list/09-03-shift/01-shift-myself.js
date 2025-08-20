97f5d0e4a85e9098f1a4b4cde7c5340ad71966ad
Miles Kim <kyw05171@gmail.com>
2025-08-20 22:30:28 +0900

    solve 09-03 / 01-shift-myself

---
+++ 02-algorithm-udemy-lecture/src/lectures/09-singly-linked-list/09-03-shift/01-shift-myself.js
@@ -0,0 +1,105 @@
+export class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+export class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    // 새로운 Node 생성하기
+    const newNode = new Node(val);
+
+    // head 가 없다면,
+    if (!this.head) {
+      // head 에 newNode 바인딩하기
+      this.head = newNode;
+      // tail 에 newNode 바인딩하기
+      this.tail = newNode;
+    }
+    // head 가 있다면,
+    else {
+      // 기존 tail 의 next 를 newNode 로 변경하기
+      this.tail.next = newNode;
+      // tail 을 newNode 로 변경하기
+      this.tail = newNode;
+    }
+
+    // 개수 +1
+    this.length++;
+
+    // instance 반환하기
+    return this;
+  }
+
+  pop() {
+    // head 가 없다면,
+    if (!this.head) {
+      // undefined 반환하기
+      return undefined;
+    }
+
+    // 현재 Node 변수 만들기
+    let current = this.head;
+    // 새로운 tail 변수 만들기
+    let newTail = this.head;
+
+    // 현재 Node 의 next 가 존재할 때까지 순회하며,
+    while (current.next) {
+      // 새로운 tail 을 현재 Node 로 변경하기
+      newTail = current;
+      // 현재 Node 변수를 현재 Node 의 next 로 변경하기
+      current = current.next;
+    }
+
+    // 개수 -1
+    this.length--;
+    // 새로운 tail 의 next 를 null 로 변경하기
+    newTail.next = null;
+    // tail 을 새로운 tail 로 갱신하기
+    this.tail = newTail;
+
+    // 현재 Node 와 새로운 tail 이 같다면,
+    if (current === newTail) {
+      // head 를 null 로 변경하기
+      this.head = null;
+      // tail 을 null 로 변경하기
+      this.tail = null;
+    }
+
+    // 현재 Node 반환하기
+    return current;
+  }
+
+  shift() {
+    // head 가 없다면,
+    if (!this.head) {
+      // undefined 반환하기
+      return undefined;
+    }
+
+    // head 를 oldHead 변수에 담기
+    const oldHead = this.head;
+    // head 를 oldHead.next 로 변경하기
+    this.head = oldHead.next;
+    // oldHead.next 를 null 로 바꾸기
+    oldHead.next = null;
+    // length -1
+    this.length--;
+
+    // 새로운 head 가 null 이라면,
+    if (!this.head) {
+      // tail 도 null 로 변경하기
+      this.tail = null;
+    }
+
+    // oldHead 반환하기
+    return oldHead;
+  }
+}
