a6d54e6d1f003d9b6a81070bde0513eb98abc705
Miles Kim <kyw05171@gmail.com>
2025-08-21 17:46:33 +0900

    study 09-09 / 02-reverse-lecture

---
+++ 02-algorithm-udemy-lecture/src/lectures/09-singly-linked-list/09-09-reverse/02-reverse-lecture.spec.js
@@ -0,0 +1,55 @@
+import { SinglyLinkedList } from './02-reverse-lecture';
+
+describe('09-09 / 02-reverse-lecture', () => {
+  it('list.reverse() => [5, 4, 3, 2, 1]', () => {
+    const list = new SinglyLinkedList();
+
+    list.push(1);
+    list.push(2);
+    list.push(3);
+    list.push(4);
+    list.push(5);
+
+    list.reverse();
+    expect(list.get(0).val).toBe(5);
+    expect(list.get(1).val).toBe(4);
+    expect(list.get(2).val).toBe(3);
+    expect(list.get(3).val).toBe(2);
+    expect(list.get(4).val).toBe(1);
+    expect(list.tail.next).toBeNull();
+    expect(list.length).toBe(5);
+  });
+
+  it('list.reverse() => [2, 1]', () => {
+    const list = new SinglyLinkedList();
+
+    list.push(1);
+    list.push(2);
+
+    list.reverse();
+    expect(list.get(0).val).toBe(2);
+    expect(list.get(1).val).toBe(1);
+    expect(list.tail.next).toBeNull();
+    expect(list.length).toBe(2);
+  });
+
+  it('list.reverse() => [1]', () => {
+    const list = new SinglyLinkedList();
+
+    list.push(1);
+
+    list.reverse();
+    expect(list.get(0).val).toBe(1);
+    expect(list.tail.next).toBeNull();
+    expect(list.length).toBe(1);
+  });
+
+  it('list.reverse() => []', () => {
+    const list = new SinglyLinkedList();
+
+    list.reverse();
+    expect(list.get(0)).toBeNull();
+    expect(list.head).toBeNull();
+    expect(list.tail).toBeNull();
+  });
+});
