282e1fbfc452d02ae6f5cddcdebee7d688570a23
Miles Kim <kyw05171@gmail.com>
2025-09-03 18:41:19 +0900

    remove mock files

--- programmers/src/level-01/fibonacci.spec.js
+++
@@ -1,8 +0,0 @@
-import { fibonacci } from './fibonacci';
-
-describe('fibonacci', () => {
-  it('fibonacci(10) === 55', () => {
-    const result = fibonacci(10);
-    expect(result).toBe(55);
-  });
-});
