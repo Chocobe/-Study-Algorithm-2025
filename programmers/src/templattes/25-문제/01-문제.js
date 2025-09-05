f9aa2dad3cc396a52e7fe2a96d20631dce3efbdb
Miles Kim <kyw05171@gmail.com>
2025-09-05 11:25:45 +0900

    create programmers algorithm practice template

---
+++ programmers/src/templattes/25-문제/01-문제.js
@@ -0,0 +1,15 @@
+/**
+ * 문제: _문제_
+ * 
+ * 난이도: _Level _
+ * 
+ * 성공여부: _성공/실패_
+ * 
+ * @link https://
+ * 
+ * @param {} a
+ * @returns {}
+ */
+export function solution(a) {
+  // 
+};
---
+++ programmers/src/templattes/25-문제/01-문제.spec.js
@@ -0,0 +1,7 @@
+import { solution } from './01-문제';
+
+describe('25 / 01-문제', () => {
+  it('solution() === a', () => {
+    // 
+  });
+});
