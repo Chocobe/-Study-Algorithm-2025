7e8b78cc87a644ac744ccadb6b4d5a22a8d108b4
Miles Kim <kyw05171@gmail.com>
2025-09-12 15:48:40 +0900

    study 250912 / 02-동영상-재생기-gpt

---
+++ programmers/src/level-01/250912-동영상-재생기/02-동영상-재생기-gpt.js
@@ -0,0 +1,64 @@
+/**
+ * 문제: _[PCCP 기출문제] 1번 / 동영상 재생기_
+ * 
+ * 난이도: _Level 1_
+ * 
+ * 성공여부: _성공/실패_
+ * 
+ * @link https://school.programmers.co.kr/learn/courses/30/lessons/340213
+ * 
+ * @param { string } video_len
+ * @param { string } pos
+ * @param { string } op_start
+ * @param { string } op_end
+ * @param { string[] } commands
+ * @returns { string }
+ */
+export function solution(
+  video_len,
+  pos,
+  op_start,
+  op_end,
+  commands
+) {
+  const videoLen = parseToSeconds(video_len);
+  const opStart = parseToSeconds(op_start);
+  const opEnd = parseToSeconds(op_end);
+  let position = parseToSeconds(pos);
+
+  const normalize = (seconds) => {
+    if (seconds < 0) {
+      seconds = 0;
+    } else if (seconds > videoLen) {
+      seconds = videoLen;
+    }
+
+    if (opStart <= seconds && seconds < opEnd) {
+      seconds = opEnd;
+    }
+
+    return seconds;
+  };
+
+  position = normalize(position);
+
+  commands.forEach(command => {
+    if (command === 'prev') {
+      position -= 10;
+    } else if (command === 'next') {
+      position += 10;
+    }
+
+    position = normalize(position);
+  });
+
+  const hours = Math.floor(position / 60);
+  const minutes = position % 60;
+
+  return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
+}
+
+function parseToSeconds(time) {
+  const [mm, ss] = time.split(':').map(v => Number(v));
+  return mm * 60 + ss;
+}
