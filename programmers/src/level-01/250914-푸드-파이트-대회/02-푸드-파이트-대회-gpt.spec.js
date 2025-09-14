254e001140c658f01ae3130cf22540497a196d86
Miles Kim <kyw05171@gmail.com>
2025-09-14 16:15:42 +0900

    study 250914 / 02-푸드-파이터-대회

---
+++ programmers/src/level-01/250914-푸드-파이트-대회/02-푸드-파이트-대회-gpt.spec.js
@@ -0,0 +1,19 @@
+import { solution } from './02-푸드-파이터-대회-gpt';
+
+/**
+food           result
+[1, 3, 4, 6]   "1223330333221"
+[1, 7, 1, 2]   "111303111"
+ */
+
+describe('250914 / 02-푸드-파이트-대회-gpt', () => {
+  it('solution() === "1223330333221"', () => {
+    const result = solution([1, 3, 4, 6]);
+    expect(result).toBe('1223330333221');
+  });
+
+  it('solution() === "111303111"', () => {
+    const result = solution([1, 7, 1, 2]);
+    expect(result).toBe('111303111');
+  });
+});
