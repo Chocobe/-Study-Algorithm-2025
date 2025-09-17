cab1bccd93033947aafb1d504dc06bfdecf4f4a2
Miles Kim <kyw05171@gmail.com>
2025-09-17 17:38:52 +0900

    rename 01-dfs-부분선택 to 01-dfs-조합-선택-비선택

--- programmers/src/templattes/dfs-template/03-dfs-조합-선택-비선택/01-dfs-조합-선택-비선택.spec.js
+++ programmers/src/templattes/dfs-template/03-dfs-조합-선택-비선택/01-dfs-조합-선택-비선택.spec.js
@@ -1,6 +1,6 @@
-import { dfsTemplate_subset } from './01-dfs-부분집합';
+import { dfsTemplate_subset } from './01-dfs-조합-선택-비선택';
 
-describe('dfs-template / 03-dfs-부분집합', () => {
+describe('dfs-template / 03-dfs-조합-선택-비선택', () => {
   it('dfsTemplate_subset([1, 2, 3], 3) === 2', () => {
     const result = dfsTemplate_subset([1, 2, 3], 3);
     expect(result).toBe(2);
